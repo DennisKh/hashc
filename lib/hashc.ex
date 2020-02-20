27efb9a9ac11805876fad9142249df58da0b36ad
Dennis <denys.kharchuk@ria.com>
2020-02-20 19:31:05 +0200

    fix

--- lib/hashc.ex
+++ lib/hashc.ex
@@ -2,7 +2,7 @@ defmodule Hashc do
   @filename
   def fileread do
     stream = File.stream!(@filename)
-    fixed_contents = stream \
+    fixed_contents = stream
     |> Enum.map(&String.trim/1)
   end
 end
