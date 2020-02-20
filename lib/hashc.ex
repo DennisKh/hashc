64b33576b1f032d7076392d6553ceebf7308dfdd
Dennis <denys.kharchuk@ria.com>
2020-02-20 19:26:42 +0200

    Initial commit

---
+++ lib/hashc.ex
@@ -0,0 +1,5 @@
+defmodule Hashc do
+  def fileread do
+
+  end
+end
---
+++ test/hashc_test.exs
@@ -0,0 +1,8 @@
+defmodule HashcTest do
+  use ExUnit.Case
+  doctest Hashc
+
+  test "greets the world" do
+    assert Hashc.hello() == :world
+  end
+end
